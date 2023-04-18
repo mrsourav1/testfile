858e2944e560c4a5cbb80a4d411675f8f15020ba
sourav <souravsahani99@gmail.com>
2023-04-18 17:04:33 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express")
 const port = 8080
 const app = express()
 
+
+app.get("/",(req,res)=>{
+    res.send("Is this Working?")
+})
+
 app.get('/api/v1/rollers', (req,res)=>{
+    console.log("THIs is working")
     res.status(200).json({message:"How are you"})
 })
 
